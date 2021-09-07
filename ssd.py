470a00f043a1eff2c2e04cd9169171b099130fa7
tomgause <71714664+tomgause@users.noreply.github.com>
2021-09-07 18:24:32 -0400

    Testing...

--- ssd.py
+++ ssd.py
@@ -182,7 +182,7 @@ class SSD(nn.Module):
         if self.phase == "test":
             output = self.detect.apply(self.num_classes, 0, 200, 0.01, 0.45,
                 loc.view(loc.size(0), -1, 4),
-                self.softmax(conf.view(conf.size(0), -1, self.num_classes)),
+                self.softmax(conf.view(-1, self.num_classes)),
                 self.priors.type(type(x.data))
             )
                 # loc.view(loc.size(0), -1, 4),                   # loc preds

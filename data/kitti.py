c6728b86946e1acc2d28e7bc46373c2205099d39
tomgause <71714664+tomgause@users.noreply.github.com>
2021-09-08 00:25:35 -0400

    Testing...

--- data/kitti.py
+++ data/kitti.py
@@ -99,13 +99,12 @@ class KittiLoader(data.Dataset):
                 self.files[split] = file_list[train_split[0]:train_split[1]]
 
             # Create list of label files
-            label_list = glob(os.path.join(root, split, 'label_2', '*.txt'))
+            label_list = glob(os.path.join(root, "training", 'label_2', '*.txt'))
             if train_split[0]==-1:
                 self.labels[split] = label_list
             else:
-                self.labels[split] = label_list[train_split[0]:train_split[1]]
-
-            print(self.labels)
+                # +1 placeholder, turns out test set doesn't have normal labels >..a.sd.
+                self.labels[split] = label_list[train_split[0]+1:train_split[1]+1]
 
             # Create list of image ids
             id_list = []
